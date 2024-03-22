0c8a85059e1699869af8febf5821c2131ae51a79
Glory_Kuria <kuriaglory@gmail.com>
2024-03-22 09:08:00 +0300

    add files

---
+++ arrays.js
@@ -0,0 +1,46 @@
+let arr1 = [3,7,34,90,12]
+console.log('arr1', arr1.slice(-1));
+
+let arr2 = [true, "green", "where",12,56]
+console.log('arr2', arr2.length - 1);
+
+
+
+//join the following array elements into a string
+
+let myPets = ["Cow", "Bird", "Snake", "Dog"];
+let joinPets = myPets.join();
+console.log({joinPets});
+
+
+//sort the following array items
+
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+
+console.log(arr3.sort(function(a, b){return a -b}));
+
+//remove duplicates from the following array.Console the array without duplicates, and console another array that only contains the duplicates
+
+let arr = ["boy", "man", "girl",            "school", "girl", "woman"];
+ function removeDuplicates(arr) {
+   return arr.filter((item,
+         index) => arr.indexOf(item) === index);
+ }
+ console.log(removeDuplicates(arr));
+
+//search for the following word in the array."food"If the word is present, return it else return "the search word was not found"
+
+let items = ["shelter", "clothing", "food", "education"];
+let i = items.includes("food");
+if(i==true){
+    console.log("food")
+}
+else{
+console.log("the search word was not found")
+}
+
+//sort the following string:
+
+let word = "renniw";
+
+console.log('split', word.split());
--- strings.js
+++ strings.js
@@ -1,40 +1,36 @@
-// extract the last four characters from the string
-let a= 'extravaganza'
+// // extract the last four characters from the string
+// let a= 'extravaganza'
+// console.log(a.slice(8,));
 
-console.log({character:a[8]});
-console.log({character:a[9]});
-console.log({character:a[10]});
-console.log({character:a[11]})
+// // insert string 'eat' at the fourth index
+// const food="The quick fox jumped over the lazy dog";
+// let b ="eat";
+// let sentence = `The ${b} quick fox jumped over the lazy dog`;
 
-// insert string 'eat' at the fourth index
-const food="The quick fox jumped over the lazy dog";
-const insert = "eat";
-const secfood = food.substring (0,4) + insert + food.substring(4);
+// console.log(sentence);
 
-console.log(secfood);
+// // count how many times the following strings appear
+// // 'the'
+// const count = "The quick fox jumped over the lazy dog"
+// const count1 =(count.match(/the/gi).length);
+// // 'brown'
+// const count2 =(count.match(/brown/gi).length);
+// console.log({count1})
+// console.log({count2})
 
-// count how many times the following strings appear
-// 'the'
-const count = "The quick fox jumped over the lazy dog"
-const count1 =(count.match(/the/gi)|| [].length);
-// 'brown'
-const count2 =(count.match(/brown/gi)|| [].length);
+// // Find words from this strings
+// // are
+// const string1 = "The pupils are reading in the library";
+// console.log({index:string1.indexOf("are")});
 
-console.log("Number of occurences of 'the':" , count1);
-console.log("Number of occurences of 'brown':" , count2 );
+// // sitting
+// const string2 = "The child was sitting on the table before it fell";
+// console.log({index:string2.indexOf("sitting")});
 
-// Find words from this strings
-// are
-const string1 = "The pupils are reading in the library";
-console.log({index:string1.indexOf("are")});
+// // convert to uppercase & lowercase
+// let d = "wonderful"
+// let c = "amazing"
 
-// sitting
-const string2 = "The child was sitting on the table before it fell";
-console.log({index:string2.indexOf("sitting")});
+// console.log(b.toUpperCase());
+// console.log(c.toLowerCase());
 
-// convert to uppercase & lowercase
-let b = "wonderful"
-let c = "amazing"
-
-console.log(b.toUpperCase());
-console.log(c.toLowerCase());
