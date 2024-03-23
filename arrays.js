cbb72ca471d99689c1cf4e24e91a85a5b6d21ca7
Glory_Kuria <kuriaglory@gmail.com>
2024-03-23 07:58:53 +0300

    add file

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 let arr1 = [3,7,34,90,12]
-console.log('arr1', arr1.slice(-1));
+console.log( arr1.slice(-1));
 
 let arr2 = [true, "green", "where",12,56]
-console.log('arr2', arr2.length - 1);
+console.log( arr2.slice (-1));
 
 
 
@@ -17,7 +17,7 @@ console.log({joinPets});
 
 var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
 
-console.log(arr3.sort(function(a, b){return a -b}));
+console.log(arr3.sort(function(a, b){return a - b}));
 
 //remove duplicates from the following array.Console the array without duplicates, and console another array that only contains the duplicates
 
@@ -30,8 +30,8 @@ let arr = ["boy", "man", "girl",            "school", "girl", "woman"];
 
 //search for the following word in the array."food"If the word is present, return it else return "the search word was not found"
 
-let items = ["shelter", "clothing", "food", "education"];
-let i = items.includes("food");
+let arr5 = ["shelter", "clothing", "food", "education"];
+let i = arr5.includes("food");
 if(i==true){
     console.log("food")
 }
@@ -42,5 +42,5 @@ console.log("the search word was not found")
 //sort the following string:
 
 let word = "renniw";
-
-console.log('split', word.split());
+let sortedString = word.sort()
+console.log( sortedString);
